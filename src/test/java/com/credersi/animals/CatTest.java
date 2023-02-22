7f0ec8803cb3309ffc0a5d793c13da36233731af
Harveymckeown <harvey.mckeown@roq.co.uk>
2023-02-22 10:36:47 +0000

    added new tests

--- src/test/java/com/credersi/animals/CatTest.java
+++ src/test/java/com/credersi/animals/CatTest.java
@@ -12,4 +12,64 @@ public class CatTest {
 		assertTrue(cat.isAlive());
 		assertEquals(cat.howHungry(), 50);
 	}
+	@Test
+	public void feedCat() {
+		Cat cat = new Cat();
+		cat.feeds();
+		assertEquals(cat.howHungry(), 30);
+	}
+	@Test
+	public void runCat() {
+		Cat cat = new Cat();
+		cat.runs();
+		assertEquals(cat.howHungry(), 80);
+	}
+	@Test
+	public void feedCatThenRunCat() {
+		Cat cat = new Cat();
+		cat.feeds();
+		cat.runs();
+		assertEquals(cat.howHungry(), 60);
+	}
+	@Test
+	public void catDiedOnce() {
+		Cat cat = new Cat();
+		cat.runs();
+		cat.runs();
+		assert(cat.isAlive() == true);
+	}
+	@Test
+	public void deadCat() {
+		Cat cat = new Cat();
+		cat.runs();
+		cat.runs();
+		//dead count 1
+		cat.runs();
+		cat.runs();
+		//dead count 2
+		cat.runs();
+		cat.runs();
+		//dead count 3
+		cat.runs();
+		cat.runs();
+		//dead count 4
+		cat.runs();
+		cat.runs();
+		//dead count 5
+		cat.runs();
+		cat.runs();
+		//dead count 6
+		cat.runs();
+		cat.runs();
+		//dead count 7
+		cat.runs();
+		cat.runs();
+		//dead count 8
+		cat.runs();
+		cat.runs();	
+		//dead count 9
+		cat.runs();
+		cat.runs();
+		assert(cat.isAlive() == false);
+	}
 }
